407c106b07b4906012ab2dab78f9d40b15326867
Mayuresh12345 <mayureshgadekar067@gmail.com>
2017-07-09 12:55:17 +0530

    First Commit

---
+++ Assets/Scripts/Background Scripts/BGScalar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScalar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer sr = GetComponent<SpriteRenderer> ();
+		Vector3 tempscale = transform.localScale;
+
+		float width = sr.sprite.bounds.size.x;
+		float worldHeight = Camera.main.orthographicSize * 2.0f;
+		float worldWidth = worldHeight / Screen.height * Screen.width;
+
+		tempscale.x = worldWidth / width;
+		transform.localScale = tempscale;
+
+	}
+	
+}
---
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 8f ,  maxvelocity = 12f;
+	[SerializeField]
+	private Rigidbody2D myBody;
+	private Animator anim;	
+	// Use this for initialization
+	void Awake()
+	{
+		myBody = GetComponent<Rigidbody2D> ();
+ 		anim = GetComponent<Animator> ();
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		PlayerMoveKeyboard ();
+	}
+
+	void PlayerMoveKeyboard ()
+	{
+		float forceX = 0;
+		float vel = Mathf.Abs (myBody.velocity.x);
+		float h = Input.GetAxisRaw ("Horizontal");			//return -1 up and left 0 for nothing 1 for right or d
+		if (h > 0) {
+			if (vel < maxvelocity) {
+				forceX = speed;				//right
+					
+			}
+			Vector3 temp = transform.localScale;
+			temp.x = 1.3f;
+			transform.localScale = temp;
+			anim.SetBool ("Walk", true);
+
+		} else if (h < 0) {
+			if (vel < maxvelocity) {
+				forceX = -speed;			//left
+		
+			}
+			Vector3 temp = transform.localScale;
+			temp.x = -1.3f;
+			transform.localScale = temp;
+
+			anim.SetBool ("Walk", true);
+		} else {
+			anim.SetBool ("Walk", false);
+		}
+		myBody.AddForce (new Vector2 (forceX,0));
+
+	}
+
+}
+// Player
